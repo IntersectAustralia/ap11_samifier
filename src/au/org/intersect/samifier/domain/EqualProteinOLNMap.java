37a7b6ca01d57c12deb49a0fd3db42f65d4ebc78
Diego Alonso <diego@intersect.org.au>
2012-10-29 09:40:34 +1100

    AP11-102: Add Equal Protein and OLN map

---
+++ src/au/org/intersect/samifier/domain/EqualProteinOLNMap.java
@@ -0,0 +1,15 @@
+package au.org.intersect.samifier.domain;
+
+public class EqualProteinOLNMap implements ProteinToOLNMap
+{
+    @Override
+    public boolean containsProtein(String protein)
+    {
+        return true;
+    }
+
+    @Override
+    public String getOLN(String protein) {
+        return protein;
+    }
+}
