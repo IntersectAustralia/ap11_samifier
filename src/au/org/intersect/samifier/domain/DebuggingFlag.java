c74b7232f0f13b7834de90e0fd57aca94b3fba4b
PrzemyslawP <przemyslaw@intersect.org.au>
2013-02-04 15:54:51 +1100

    Updating tests - adding new field queryid

--- src/au/org/intersect/samifier/domain/DebuggingFlag.java
+++ src/au/org/intersect/samifier/domain/DebuggingFlag.java
@@ -5,7 +5,7 @@ public class DebuggingFlag {
     /// Change by Ignatius Pang  *%*%*%
     /// This debug flag is currently set to provide internal validation alternatively spliced peptides. 
     /// The nucleotide sequence in the 'output' SAM file is compared with the amino acid sequence in the 'input' Mascot DAT or mzIdentML file.
-	private static int sbi_debug = 1;
+	private static int sbi_debug = 0;
 
 	
 	public static int get_sbi_debug_flag()
